c6a0198af01f9f3aca8155a9144d6e1644034c29
lianyuxiao <1049146413@qq.com>
2025-08-29 17:07:32 +0800

    "改产品页"

--- src/components/Pc/Products/products.js
+++ src/components/Pc/Products/products.js
@@ -1,7 +1,7 @@
-let products = [
+let boxProducts = [
   {
     imgUrl: require('@/assets/img/mainProduct/pro_one.jpg'),
-    desc: '1',
+    desc: 'Luxury suede/velvet box with customized gold mirror acrylic card invitation',
     childrens: [
         {
             imgUrl: require('@/assets/img/mainProduct/pro_detail_1.jpg'),
@@ -23,8 +23,79 @@ let products = [
         }
     ]
   },
-
+  {
+    imgUrl: require('@/assets/img/mainProduct/pro_two.jpg'),
+    desc: 'Dark green suede box invitation gold foil clear acrylic wedding invitation card',
+    childrens: [
+        {
+            imgUrl: require('@/assets/img/mainProduct/pro_two_1.jpg'),
+        },
+        {
+            imgUrl: require('@/assets/img/mainProduct/pro_two_2.jpg'),
+        },
+        {
+            imgUrl: require('@/assets/img/mainProduct/pro_two_3.jpg'),
+        },
+        {
+            imgUrl: require('@/assets/img/mainProduct/pro_two_4.jpg'),
+        },
+        {
+            imgUrl: require('@/assets/img/mainProduct/pro_two_5.jpg'),
+        },
+        {
+            imgUrl: require('@/assets/img/mainProduct/pro_two_6.jpg'),
+        }
+    ]
+  },
+  {
+    imgUrl: require('@/assets/img/mainProduct/pro_three.jpg'),
+    desc: 'Baby pink velvet hardcover box with creative shape acrylic invitation card',
+    childrens: [
+        {
+            imgUrl: require('@/assets/img/mainProduct/pro_three_1.jpg'),
+        },
+        {
+            imgUrl: require('@/assets/img/mainProduct/pro_three_2.jpg'),
+        },
+        {
+            imgUrl: require('@/assets/img/mainProduct/pro_three_3.jpg'),
+        },
+        {
+            imgUrl: require('@/assets/img/mainProduct/pro_three_4.jpg'),
+        },
+        {
+            imgUrl: require('@/assets/img/mainProduct/pro_three_5.jpg'),
+        },
+        {
+            imgUrl: require('@/assets/img/mainProduct/pro_three_6.jpg'),
+        }
+    ]
+  },
+  {
+    imgUrl: require('@/assets/img/mainProduct/pro_four.jpg'),
+    desc: 'Creative shape cleae acrylic invitations with ivory suede foiling logo box ',
+    childrens: [
+        {
+            imgUrl: require('@/assets/img/mainProduct/pro_four_1.jpg'),
+        },
+        {
+            imgUrl: require('@/assets/img/mainProduct/pro_four_2.jpg'),
+        },
+        {
+            imgUrl: require('@/assets/img/mainProduct/pro_four_3.jpg'),
+        },
+        {
+            imgUrl: require('@/assets/img/mainProduct/pro_four_4.jpg'),
+        },
+        {
+            imgUrl: require('@/assets/img/mainProduct/pro_four_5.jpg'),
+        },
+        {
+            imgUrl: require('@/assets/img/mainProduct/pro_four_6.jpg'),
+        }
+    ]
+  },
 ]
 export function productList () {
-    return products;
+    return boxProducts;
 };
