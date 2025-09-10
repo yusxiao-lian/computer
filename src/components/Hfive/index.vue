9aa9861155ee7b1d245580143bdfccc3797b8faa
lianyuxiao <1049146413@qq.com>
2025-09-10 17:24:40 +0800

    "改h5"

--- src/components/Hfive/index.vue
+++ src/components/Hfive/index.vue
@@ -1,3 +1,23 @@
 <template>
-    <div>h5</div>
+    <div class="content-box">
+        <div class="menu-box">
+            <img class="logo" src="@/assets/img/home/logo.png" alt="">
+        </div>
+        <div class="content_main">
+            <router-view></router-view>
+        </div>
+    </div>
 </template>
+<script>
+
+</script>
+<style lang="less" scoped>
+.content-box {
+    .menu-box {
+        .logo {
+            width: 80px;
+            height: 50px;
+        }
+    }
+}
+</style>
